5a9cbbe9040d8bc816c8479d8ac94aa12d8541f2
张乾泽 <qianze.zhang@hotmail.com>
2018-02-01 16:06:58 +0800

    fixes

--- deploy_cn.sh
+++ deploy_cn.sh
@@ -8,6 +8,7 @@ ServerName=${@:$OPTIND:1}
 
 # echo $Rev
 # cd dist
+cp modules/config_cn.js modules/config.js
 rsync -v -z -r --progress -h --exclude=node_modules/* --exclude=.* --exclude=deploy_test.sh . $ServerName:$deployDir
 
 echo service deployed on:$ServerName:$deployDir
---
+++ deploy_en.sh
@@ -0,0 +1,2 @@
+cp modules/config_en.js modules/config.js
+git push heroku master
